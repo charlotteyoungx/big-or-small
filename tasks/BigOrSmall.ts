54bd3a4db5542ce112d87888bd2118fd6bb0016d
charlotteyoungx <vxgrezv960@hotmail.com>
2025-10-17 19:15:57 +0800

    depoly and tasks

---
+++ tasks/BigOrSmall.ts
@@ -0,0 +1,55 @@
+import { task } from "hardhat/config";
+import type { TaskArguments } from "hardhat/types";
+
+task("bos:address", "Prints the BigOrSmall address").setAction(async function (_: TaskArguments, hre) {
+  const { deployments } = hre;
+  const d = await deployments.get("BigOrSmall");
+  console.log("BigOrSmall address is " + d.address);
+});
+
+task("bos:start", "Start a round with encrypted dice")
+  .addParam("roundid", "Round id (bytes32 hex)")
+  .addParam("dice", "Dice number 1..6")
+  .setAction(async function (args: TaskArguments, hre) {
+    const { ethers, deployments, fhevm } = hre;
+    await fhevm.initializeCLIApi();
+    const dep = await deployments.get("BigOrSmall");
+    const [signer] = await ethers.getSigners();
+
+    const c = await ethers.getContractAt("BigOrSmall", dep.address);
+
+    const enc = await fhevm
+      .createEncryptedInput(dep.address, signer.address)
+      .add8(parseInt(args.dice))
+      .encrypt();
+
+    const tx = await c.connect(signer).startGame(args.roundid, enc.handles[0], enc.inputProof);
+    console.log("tx:", tx.hash);
+    await tx.wait();
+  });
+
+task("bos:bet", "Place bet")
+  .addParam("roundid", "Round id")
+  .addParam("choice", "1 small, 2 big")
+  .addParam("value", "wei value to bet")
+  .setAction(async function (args: TaskArguments, hre) {
+    const { ethers, deployments } = hre;
+    const dep = await deployments.get("BigOrSmall");
+    const [signer] = await ethers.getSigners();
+    const c = await ethers.getContractAt("BigOrSmall", dep.address);
+    const tx = await c.connect(signer).placeBet(args.roundid, parseInt(args.choice), { value: args.value });
+    console.log("tx:", tx.hash);
+    await tx.wait();
+  });
+
+task("bos:reveal", "Reveal result")
+  .addParam("roundid", "Round id")
+  .setAction(async function (args: TaskArguments, hre) {
+    const { ethers, deployments } = hre;
+    const dep = await deployments.get("BigOrSmall");
+    const [signer] = await ethers.getSigners();
+    const c = await ethers.getContractAt("BigOrSmall", dep.address);
+    const tx = await c.connect(signer).reveal(args.roundid);
+    console.log("tx:", tx.hash);
+    await tx.wait();
+  });
